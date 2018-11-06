76e19cd2c61c4d4b4d3e0b1df72f9383764728d6
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2018-11-05 21:39:26 -0800

    Update index

--- scripts/signup/index.jsx
+++ scripts/signup/index.jsx
@@ -26,8 +26,36 @@ const defaultState = {
 class SignupPageInternal extends React.Component<SignupPageProps, SignupPageState> {
   state = defaultState;
 
+  initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    organizationCompany: '',
+    organizationLocation: '',
+    organizationCompanyUrl: '',
+  }
+
+  schema = yup.object().shape({
+    name: yup.string().required('Required'),
+    email: yup.string().email().required('Required'),
+    password: yup.string().required('Required'),
+    password2: yup.string().oneOf(
+      [yup.ref('password'), null],
+      'Passwords does not match.',
+    ).required('Required'),
+    organizationCompany: yup.string(),
+    organizationLocation: yup.string(),
+    organizationCompanyUrl: yup.string().url(),
+  });
+
+
   onSubmitHandler = (values, state) => {
-    const { setSubmitting } = state;
+    const {
+      setSubmitting,
+      setErrors,
+    } = state;
+    console.log('onSubmitHandler state', state);
     const {
       name, email, password, organizationCompany, organizationLocation, organizationCompanyUrl,
     } = values;
@@ -39,8 +67,52 @@ class SignupPageInternal extends React.Component<SignupPageProps, SignupPageStat
       organization_location: organizationLocation,
       organization_company_url: organizationCompanyUrl,
     };
-    apis.accounts.signup(data)
-      .finally(() => setSubmitting(false));
+    apis.accounts.signup(data).catch((e) => {
+      // make sure status code matches
+      const {
+        name: nameErrors,
+        email: emailErrors,
+        password: passwordErrors,
+        organization_company: organizationCompanyErrors,
+        organization_location: organizationLocationErrors,
+        organization_company_url: organizationCompanyUrlErrors,
+      } = e.response.data;
+      const errors = {};
+
+      if (nameErrors && nameErrors.length > 0) {
+        const [error] = nameErrors;
+        errors.name = error;
+      }
+
+      if (emailErrors && emailErrors.length > 0) {
+        const [error] = emailErrors;
+        errors.email = error;
+      }
+
+      if (passwordErrors && passwordErrors.length > 0) {
+        const [error] = passwordErrors;
+        errors.password = error;
+      }
+
+      if (organizationCompanyErrors && organizationCompanyErrors.length > 0) {
+        const [error] = organizationCompanyErrors;
+        errors.organizationCompany = error;
+      }
+
+      if (organizationLocationErrors && organizationLocationErrors.length > 0) {
+        const [error] = organizationLocationErrors;
+        errors.organizationLocation = error;
+      }
+
+      if (organizationCompanyUrlErrors && organizationCompanyUrlErrors.length > 0) {
+        const [error] = organizationCompanyUrlErrors;
+        errors.organizationCompanyUrl = error;
+      }
+
+      setErrors(errors);
+
+      console.log(e);
+    }).finally(() => setSubmitting(false));
 
     // console.log('Signin up:', name, email, password, company, location, companyUrl);
   }
@@ -61,37 +133,14 @@ class SignupPageInternal extends React.Component<SignupPageProps, SignupPageStat
       return (<Redirect to="/" />);
     }
 
-    const initialValues = {
-      name: '',
-      email: '',
-      password: '',
-      password2: '',
-      organizationCompany: '',
-      organizationLocation: '',
-      organizationCompanyUrl: '',
-    };
-
-    const validationSchema = yup.object().shape({
-      name: yup.string().required('Required'),
-      email: yup.string().email().required('Required'),
-      password: yup.string().required('Required'),
-      password2: yup.string().oneOf(
-        [yup.ref('password'), null],
-        'Passwords does not match.',
-      ).required('Required'),
-      organizationCompany: yup.string(),
-      organizationLocation: yup.string(),
-      organizationCompanyUrl: yup.string().url(),
-    });
-
     return (
       <div className="container">
         <div className="section">
           <div className="row">
             <div className="col offset-s3 s6">
               <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
+                initialValues={this.initialValues}
+                validationSchema={this.schema}
                 onSubmit={this.onSubmitHandler}
               >
                 {(props) => {
